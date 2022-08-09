959432ac734a22536664394a198d553a3823ce2f
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2022-08-09 11:01:43 -0500

    tidytuesday week 32

--- scripts/tidy_tuesday/week_32.R
+++ scripts/tidy_tuesday/week_32.R
@@ -8,12 +8,16 @@ d <- d$wheels %>%
   select(-...1)
 
 d %>% 
-  group_by(country) %>% 
-  summarise(across(where(is.numeric), ~median(.x, na.rm=T)),
-            country = paste0(country, " (n=", n(), ")")) %>% 
-  pivot_longer(-country, names_to = "var") %>% 
+  filter(status == "Operating") %>% 
+  select(country, where(is.numeric)) %>% 
+  pivot_longer(-country) %>% 
   filter(!is.na(value)) %>% 
-  mutate(var = factor(var) %>% 
+  group_by(name, country) %>% 
+  summarise(med_value = median(value),
+            n = n()) %>% 
+  ungroup() %>% 
+  mutate(country = paste0(country, " (n=", n, ")"),
+         name = factor(name) %>% 
            fct_recode("Diameter" = "diameter",
                       "Height" = "height",
                       "Hourly Capacity" = "hourly_capacity",
@@ -22,16 +26,18 @@ d %>%
                       "Ride Duration (mins)" = "ride_duration_minutes",
                       "Seating Capacity" = "seating_capacity",
                       "Turns" = "turns")) %>% 
-  ggplot(aes(x = value, 
-             y = drlib::reorder_within(country, by = value, within = var),
-             yend = drlib::reorder_within(country, by = value, within = var))) +
+  ggplot(aes(x = med_value, 
+             y = drlib::reorder_within(country, by = med_value, within = name),
+             yend = drlib::reorder_within(country, by = med_value, within = name))) +
   geom_point(color = "grey90") +
   geom_segment(xend = 0, color = "grey90") +
   drlib::scale_y_reordered() +
-  facet_wrap(vars(var), scales = "free", nrow = 3) +
+  facet_wrap(vars(name), scales = "free", nrow = 3) +
   theme_mcm_dark() +
   theme(axis.title = element_blank()) +
   labs(title = "Big wheel keep on turning",
        subtitle = "Median values of various metrics for ferris wheels in each country")
 
-ggsave("images/tidy_tuesday/week_32_ferris_wheel_summaries.jpg", width = 10, height = 13, device = grDevices::jpeg)
+ggsave("images/tidy_tuesday/week_32_ferris_wheel_summaries.jpg", width = 10, height = 12, device = grDevices::jpeg)
+
+
